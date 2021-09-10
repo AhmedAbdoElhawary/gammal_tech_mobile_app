624cdf72580a180e7e9db6afdee3efd839f57acc
ahmed abdo <ahmedabdoelhawari12345work@gmail.com>
2021-09-10 03:35:17 +0200

    launch the phone number

--- lib/FAQ_video_page.dart
+++ lib/FAQ_video_page.dart
@@ -5,6 +5,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/rendering.dart';
 import 'package:gammal_tech_mobile_app/common_ui/common-ui.dart';
 import 'package:gammal_tech_mobile_app/courses_page.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class faqVideoPage extends StatefulWidget {
   String index;
@@ -45,65 +46,75 @@ class _faqVideoPageState extends State<faqVideoPage> {
     );
   }
 
-  SingleChildScrollView buildContainer(Map<String, dynamic> taskData, BuildContext context) {
-    return SingleChildScrollView(
-      child: Expanded(
-        child: Column(
-          children: [
-            Container(
-                  width: double.infinity,
-                  color: Color.fromARGB(215, 0, 118, 125),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        TheHeadCardOfText(textIndex),
-                        buildTheVideo(taskData),
-                        buildContainerOfText(),
-                        buildTextButton("  View Courses  ", CoursesPage(), context),
-                      ],
-                    ),
-                  ),
-                ),
-            buildTheBottomContainer(),
-          ],
+  Column buildContainer(
+      Map<String, dynamic> taskData, BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: Container(
+            width: double.infinity,
+            color: Color.fromARGB(215, 11, 108, 108),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TheHeadCardOfText(textIndex),
+                  buildTheVideo(taskData),
+                  buildContainerOfText(),
+                  buildTextButton("  View Courses  ", CoursesPage(), context),
+                ],
+              ),
+            ),
+          ),
         ),
-      ),
+        buildTheBottomContainer(),
+      ],
     );
   }
 
   Directionality buildContainerOfText() {
     return Directionality(
-                textDirection: TextDirection.rtl,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4),
-                    color: Colors.white,
-                  ),
-                  width: double.infinity,
-                  padding: const EdgeInsets.all(10),
-                  margin: const EdgeInsets.all(10),
-                  child: Container(
-                    alignment: Alignment.center,
-                    child: buildTextQuestion(),
-                  ),
-                ),
-              );
+      textDirection: TextDirection.rtl,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          color: Colors.white,
+        ),
+        width: double.infinity,
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
+        child: Container(
+          alignment: Alignment.center,
+          child: buildTextQuestion(),
+        ),
+      ),
+    );
   }
 
   SizedBox buildSizedBox() => SizedBox(height: 15);
 
-  Text buildTextQuestion() => Text(
-        "إذا واجهتك مشكلة آثناء إستخدام الموقع برجاء الإتصال بنا في أقرب وقت"
-        "\n"
-        "\n"
-        "أوقات العمل الرسمية من الأحد إلي الخميس من الساعة التاسعة صباحا وحتي الخامسة مساءا بتوقيت القاهرة"
-        "\n"
-        "\n"
-        "\n"
-        "رقم التليفون والواتساب"
-        "\n"
-        "+201033998844",
-        style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.right,
+  Directionality buildTextQuestion() => Directionality(
+        textDirection: TextDirection.ltr,
+        child: Column(
+          children: [
+            Text(
+              "إذا واجهتك مشكلة آثناء إستخدام الموقع برجاء الإتصال بنا في أقرب وقت"
+              "\n"
+              "\n"
+              "أوقات العمل الرسمية من الأحد إلي الخميس من الساعة التاسعة صباحا وحتي الخامسة مساءا بتوقيت القاهرة"
+              "\n"
+              "\n"
+              "\n"
+              "رقم التليفون والواتساب",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+            InkWell(
+              onTap: ()=>launch("tel:+201033998844"),
+                child: Text(
+              "+201033998844",
+              style: TextStyle(color: Colors.blue[700]),
+            )),
+          ],
+        ),
       );
 }
