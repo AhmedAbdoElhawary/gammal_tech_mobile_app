1bbbe8dd7ade7f0548fa90503cb23fd8458cb4d6
ahmed abdo <ahmedabdoelhawari12345work@gmail.com>
2021-09-12 23:33:43 +0200

    moving the logic methods to the provider

--- lib/common_ui/common-ui.dart
+++ lib/common_ui/common-ui.dart
@@ -1,100 +1,13 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:gammal_tech_mobile_app/FAQ_page.dart';
-import 'package:gammal_tech_mobile_app/contact_us.dart';
-import 'package:gammal_tech_mobile_app/courses_page.dart';
-import 'package:gammal_tech_mobile_app/get_the_video.dart';
-import 'package:gammal_tech_mobile_app/account_page.dart';
-import 'package:gammal_tech_mobile_app/sign_in_page.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:video_player/video_player.dart';
+import 'package:gammal_tech_mobile_app/the_pages/account_page.dart';
+import 'package:gammal_tech_mobile_app/the_pages/sign_in_page.dart';
+import 'package:vimeo_player_flutter/vimeo_player_flutter.dart';
 
-String? textOfButton;
 User? user = FirebaseAuth.instance.currentUser;
-bool checkBack = false;
-AppBar buildAppBar(context) {
-  return AppBar(
-    elevation: 5,
-    title: TextButton(
-      onPressed: () {},
-      child: Text(
-        "Gammal Tech",
-        style: TextStyle(color: Colors.teal[800], fontSize: 30),
-      ),
-    ),
-    actions: [
-      IconButton(
-        onPressed: () async {
-          SharedPreferences prefs = await SharedPreferences.getInstance();
-          textOfButton = (prefs.getString("textOfButton") == null
-              ? "Sign In"
-              : prefs.getString("textOfButton"));
-          showGeneralDialog(
-            context: context,
-            barrierDismissible: true,
-            transitionDuration: Duration(milliseconds: 500),
-            barrierLabel: MaterialLocalizations.of(context).dialogLabel,
-            barrierColor: Colors.black.withOpacity(0),
-            pageBuilder: (context, _, __) {
-              return SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 52.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        width: double.infinity,
-                        color: Color.fromARGB(0, 255, 255, 255),
-                        child: Card(
-                          color: Color.fromARGB(240, 255, 255, 255),
-                          margin: const EdgeInsets.only(),
-                          child: ListView(
-                            shrinkWrap: true,
-                            children: <Widget>[
-                              ListTile(title: Text('Premium')),
-                              listTileOfDropMenu(
-                                  context, 'Courses', CoursesPage()),
-                              ListTile(title: Text('Masterclass')),
-                              listTileOfDropMenu(context, 'FAQ', faqPage()),
-                              listTileOfDropMenu(context, 'Content', ContactUS()),
-                              biuldTextButton(context)
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-            transitionBuilder: (context, animation, secondaryAnimation, child) {
-              return SlideTransition(
-                position: CurvedAnimation(
-                  parent: animation,
-                  curve: Curves.easeOut,
-                ).drive(Tween<Offset>(
-                  begin: Offset(0, -1.0),
-                  end: Offset.zero,
-                )),
-                child: child,
-              );
-            },
-          );
-        },
-        icon: Icon(
-          Icons.menu,
-          textDirection: TextDirection.rtl,
-          color: Colors.black87,
-        ),
-      ),
-    ],
-  );
-}
-
-Center biuldTextButton(BuildContext context) {
+Center biuldTextButton(BuildContext context, bool checkAccount) {
   return Center(
     child: Card(
       color: Color.fromARGB(255, 0, 118, 125),
@@ -102,54 +15,16 @@ Center biuldTextButton(BuildContext context) {
       elevation: 5,
       child: InkWell(
         onTap: () async {
-          SharedPreferences prefs = await SharedPreferences.getInstance();
-          CollectionReference _collectionRef =
-              FirebaseFirestore.instance.collection('users');
-          QuerySnapshot querySnapshot = await _collectionRef.get();
-          int i=0;
-          for (i = 0; i < querySnapshot.docs.length&&user!=null; i++) {
-            if (querySnapshot.docs[i]["phone"] == user!.phoneNumber) break;
-          }
-          var data = querySnapshot.docs[i];
-          print(
-              "=============================================\n${querySnapshot.docs[i].data()}\n${data.get("name")}\n==============================================");
-          TextEditingController controlUserName = TextEditingController();
-          controlUserName.text = data.get("name");
-          TextEditingController controlUserEmail = TextEditingController();
-          controlUserEmail.text = data.get("email");
-          String dropdownValueShirtSize =
-              data.get("shirt") == "" ? 'Select one...' : data.get("shirt");
-          String dropdownValueGender =
-              data.get("gender") == "" ? 'Select one...' : data.get("gender");
-          String dropdownValueDay =
-              data.get("birthday") == "" ? '4' : data.get("birthday");
-          String dropdownValueMonth =
-              data.get("birthmonth") == "" ? '4' : data.get("birthmonth");
-          String dropdownValueYear =
-              data.get("birthyear") == "" ? '2000' : data.get("birthyear");
-          String id = data.id;
-
           Navigator.push(
               context,
               MaterialPageRoute(
                   builder: (context) =>
-                      prefs.getString("textOfButton") == "SIGN IN"
-                          ? signInPage()
-                          : accountPage(
-                              dropdownValueDay: dropdownValueDay,
-                              dropdownValueGender: dropdownValueGender,
-                              dropdownValueMonth: dropdownValueMonth,
-                              dropdownValueShirtSize: dropdownValueShirtSize,
-                              dropdownValueYear: dropdownValueYear,
-                              controlUserName: controlUserName,
-                              controlUserEmail: controlUserEmail,
-                              docId: id,
-                            )));
+                      checkAccount != true ? signInPage() : accountPage()));
         },
         child: Container(
           padding: EdgeInsets.all(8),
           child: Text(
-            textOfButton!,
+            checkAccount ? 'My Account' : 'Sign In',
             style: TextStyle(
                 fontSize: 15, fontWeight: FontWeight.w400, color: Colors.white),
           ),
@@ -202,18 +77,18 @@ Card buildTextButton(String text, page, context) {
   );
 }
 
-Padding buildTheVideo(Map<String, dynamic> taskData) {
+Padding buildTheVideo(bool checkForPadding,bool checkForHeight) {
+  const videoId = '70591644';
   return Padding(
-    padding: const EdgeInsets.only(top: 15, right: 15, left: 15),
+    padding:checkForPadding?const EdgeInsets.only(top: 15, right:10, left: 10):const EdgeInsets.only(top: 15, right:15, left: 15),
     child: Container(
-      color: Colors.white,
+        color: Colors.white,
       width: double.infinity,
-      height: 186,
-      child: ChewieListItem(
-        videoPlayerController:
-            VideoPlayerController.network(taskData["videoUrl"]),
-        looping: true,
-      ),
+      height:checkForHeight?170:186,
+      child: VimeoPlayer(videoId: videoId),
+      // child: VimeoPlayer(
+      //   videoId: videoId,
+      // ),
     ),
   );
 }
@@ -234,58 +109,4 @@ Card TheHeadCardOfText(String title) {
       ),
     ),
   );
-}
-
-Container buildTheBottomContainer() {
-  return Container(
-    width: double.infinity,
-    height: 33,
-    color: Colors.white,
-    child: Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          buildIconYoutube(),
-          buildTextYoutube(),
-        ],
-      ),
-    ),
-  );
-}
-
-InkWell buildTextYoutube() {
-  return InkWell(
-    onTap: () {
-      launchURL("https://www.youtube.com/gammaltech?sub_confirmation=1");
-    },
-    child: Text(
-      "© 2021 Gammal Tech. All rights reserved.",
-      style: TextStyle(
-        fontSize: 15,
-        fontWeight: FontWeight.w400,
-      ),
-    ),
-  );
-}
-
-InkWell buildIconYoutube() {
-  return InkWell(
-    onTap: () {
-      launchURL("https://www.youtube.com/gammaltech?sub_confirmation=1");
-    },
-    child: Image.asset(
-      "lib/asset/images/youtube.png",
-      fit: BoxFit.cover,
-      width: 35,
-      height: 70,
-    ),
-  );
-}
-
-launchURL(url) async {
-  if (await canLaunch(url)) {
-    await launch(url);
-  } else {
-    showToast('Could not launch : $url');
-  }
 }
