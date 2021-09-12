a0124adb545e648daa3d8b39d3cc3df94ec5ea72
ahmed abdo <ahmedabdoelhawari12345work@gmail.com>
2021-09-13 00:01:43 +0200

    add Provider_SignIn

---
+++ lib/provider_classes/provider_send_email.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
+import 'package:gammal_tech_mobile_app/common_ui/common-ui.dart';
+import 'package:image_picker/image_picker.dart';
+
+class Provider_SendEmail extends ChangeNotifier {
+  TextEditingController controlUserName = TextEditingController();
+  TextEditingController controlPhoneNumber = TextEditingController();
+  TextEditingController controlEmailAddress = TextEditingController();
+  TextEditingController controlMessage = TextEditingController();
+  List<String> attachments = [];
+  Future<void> send(context) async {
+    final Email email = Email(
+      cc: [controlEmailAddress.text],
+      bcc: [controlUserName.text],
+      body: controlMessage.text,
+      subject: "${controlUserName.text}\n${controlPhoneNumber.text}",
+      recipients: ["ahmedabdoelhawari12345asd@gmail.com"],
+      attachmentPaths: attachments,
+    );
+    String platformResponse;
+
+    try {
+      await FlutterEmailSender.send(email);
+      platformResponse = 'success';
+    } catch (error) {
+      platformResponse = error.toString();
+    }
+    showToast(platformResponse);
+    notifyListeners();
+  }
+
+  void openImagePicker() async {
+    final picker = ImagePicker();
+    PickedFile? pick = await picker.getImage(source: ImageSource.gallery);
+    if (pick != null) {
+        attachments.add(pick.path);
+    }
+    notifyListeners();
+  }
+
+  void removeAttachment(int index) {
+      attachments.removeAt(index);
+      notifyListeners();
+  }
+}
