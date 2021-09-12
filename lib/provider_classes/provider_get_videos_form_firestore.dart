e5b2fc8e33d7ae4c5f8377629444918f5c06512e
ahmed abdo <ahmedabdoelhawari12345work@gmail.com>
2021-09-13 00:55:24 +0200

    add Provider_GetTheVideosFromFirestore

---
+++ lib/provider_classes/provider_get_videos_form_firestore.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:gammal_tech_mobile_app/the_pages/video_page.dart';
+import 'package:gammal_tech_mobile_app/the_pages/waitingPage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Provider_GetTheVideosFromFirestore extends ChangeNotifier {
+  onPressedLessonButton(context, index) {
+    SchedulerBinding.instance!.addPostFrameCallback((_) async {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      Navigator.push(context, MaterialPageRoute(builder: (context) {
+        if (prefs.getBool("checkForAccount") == true ||
+            index == 0 ||
+            index == 1 ||
+            index == 2) return videoPage(index, prefs);
+        return waitingPage(image: null,checkAnswer: false,checkAccount: false,);
+      }));
+    });
+  }
+}
