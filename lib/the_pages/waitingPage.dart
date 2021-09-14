688a6265aac9524832e69937a715c82f691a7d0e
ahmed abdo <ahmedabdoelhawari12345work@gmail.com>
2021-09-14 21:42:40 +0200

    handling sign in & sign out cases and another cases

--- lib/the_pages/waitingPage.dart
+++ lib/the_pages/waitingPage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:gammal_tech_mobile_app/the_pages/lesson_page.dart';
+import 'package:gammal_tech_mobile_app/the_pages/sign_in_page.dart';
 
 class waitingPage extends StatefulWidget {
   var image;
@@ -9,14 +11,10 @@ class waitingPage extends StatefulWidget {
   waitingPage({required this.image, required this.checkAnswer,required this.checkAccount});
   @override
   State<waitingPage> createState() =>
-      _waitingPageState(image: image, checkAnswer: checkAnswer,checkAccount:checkAccount );
+      _waitingPageState();
 }
 
 class _waitingPageState extends State<waitingPage> {
-  var image;
-  bool checkAnswer;
-  bool checkAccount;
-  _waitingPageState({required this.image, required this.checkAnswer,required this.checkAccount});
   int _start = 5;
   @override
   void dispose() {
@@ -33,22 +31,39 @@ class _waitingPageState extends State<waitingPage> {
         color: Color.fromARGB(215, 11, 108, 108),
         child: Center(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            if (image != null) Expanded(flex: 6, child: Image.asset(image)),
-            Expanded(flex: 1, child: Center(child: Text("$_start"))),
+            if (widget.image != null) Column(
+              children: [
+                Image.asset(widget.image),
+                buildText(widget.checkAnswer?"Correct Answer ! ":"Wrong Answer"),
+              ],
+            ),
+            if (!widget.checkAccount) buildText("Sign in first"),
+            SizedBox(height: 10,),
+            Center(child:buildText("$_start"),
+            ),
           ],
         )),
       ),
     );
   }
+
+  Text buildText(String text) => Text(text,style: TextStyle(color: Colors.white,fontSize: 18));
   Future<void> startTimer() async {
     WidgetsBinding.instance!.addPostFrameCallback((_) async {
       if (_start <= 0) {
-        if(checkAnswer)
-          Navigator.push(context, MaterialPageRoute(
-              builder: (context) => lessonPage('C Programming')));
-        else if(!checkAccount||!checkAnswer)
-          Navigator.pop(context);
+        if(widget.checkAccount){
+          if(widget.checkAnswer)
+            Navigator.push(context, MaterialPageRoute(
+                builder: (context) => lessonPage('C Programming')));
+          else if(!widget.checkAnswer)
+            Navigator.pop(context);
+        }
+        else{
+          Navigator.push(context,  MaterialPageRoute(
+              builder: (context) => signInPage()));
+        }
       } else {
         await Future.delayed(const Duration(seconds: 1), () {
           WidgetsBinding.instance!.addPostFrameCallback((_){
